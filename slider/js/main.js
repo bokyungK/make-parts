e8566791d4c411980a99cec8685a3d78cd73f839
bokyungK <freebob2b@gmail.com>
2022-05-26 15:21:15 +0900

    change some css styles and add button on/off

--- slider/js/main.js
+++ slider/js/main.js
@@ -3,26 +3,55 @@ const buttonRight = document.getElementsByClassName('button-right')[0];
 const cardContainer = document.getElementsByClassName('card-container')[0];
 const liList = document.getElementsByClassName('card-item');
 let moveData = 0;
+buttonLeft.style.backgroundColor = 'lightgray';
 
 function moveRight(event) {
     if (moveData < 0) {
         const buttonLeft = event.target;
         moveData = moveData + 160;
 
+        buttonRight.style.backgroundColor = 'gold';
+        buttonRight.style.transition = 'background-color 1s';
+        // buttonRight.removeEventListener('click', moveLeft);
+
         cardContainer.style.transition = 'transform 1s';
         cardContainer.style.transform = 'translateX(' + String(moveData) + 'px';
+
+        if (moveData === 0) {
+            buttonLeft.style.backgroundColor = 'lightgray';
+            buttonLeft.style.transition = 'background-color 1s';
+            // buttonLeft.addEventListener('click', moveRight)
+
+            buttonRight.style.backgroundColor = 'gold';
+            buttonRight.style.transition = 'background-color 1s';
+            // buttonRight.removeEventListener('click', moveLeft);
+        }
     }
 }
 
 function moveLeft(event) {
-    if ((liList.length * 160) + moveData > 800) {
+    if ((liList.length * 160) + moveData + 10 > 810) {
         const buttonRight = event.target;
         moveData = moveData + (-160);
 
         cardContainer.setAttribute('data-position', moveData);
         cardContainer.style.transition = 'transform 1s';
         cardContainer.style.transform = 'translateX(' + String(moveData) + 'px';
-    }   
+
+        buttonLeft.style.backgroundColor = 'gold';
+        buttonLeft.style.transition = 'background-color 1s';
+        // buttonLeft.addEventListener('click', moveRight)
+
+        if ((liList.length * 160) + moveData + 10 <= 810) {
+            buttonRight.style.backgroundColor = 'lightgray';
+            buttonRight.style.transition = 'background-color 1s';
+            // buttonRight.removeEventListener('click', moveLeft);
+
+            buttonLeft.style.backgroundColor = 'gold';
+            buttonLeft.style.transition = 'background-color 1s';
+            // buttonLeft.addEventListener('click', moveRight)
+        }
+    }
 }
 
 buttonLeft.addEventListener('click', moveRight)
