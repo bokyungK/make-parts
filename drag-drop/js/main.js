a213249f94022fa4e93f725763b34f39cb1edf23
bokyungK <freebob2b@gmail.com>
2022-05-31 15:31:55 +0900

    block left and right move

--- drag-drop/js/main.js
+++ drag-drop/js/main.js
@@ -1,24 +1,33 @@
+// 슬라이더 드래그 조작
+
 const cardItems = document.getElementsByClassName('card-item');
 const cardContainer = document.getElementsByClassName('card-container')[0];
 let moveData = 0;
 let mouseValue = 0;
 let startTouchX = 0;
+let position = 0;
+cardContainer.style.transition = 'transform 0s';
 
 function touchEnd(event) {
     event.preventDefault();
 
     mouseValue = 0;
-    moveData = 0;
+    position = moveData;
 
-    cardContainer.style.transition = 'transform 1s';
-    cardContainer.style.transform = 'translateX(0)';
+    if (position > 0) {
+        cardContainer.style.transition = 'transform 1s';
+        cardContainer.style.transform = 'translate(0)'
+    } else if (position < -480) {
+        cardContainer.style.transition = 'transform 1s';
+        cardContainer.style.transform = 'translate(-480px)';
+    }
 }
 
 function touchMove(event) {
     event.preventDefault();
 
     if (mouseValue === 1) {
-        moveData = event.clientX - startTouchX;
+        moveData = (event.clientX - startTouchX);
         cardContainer.style.transform = 'translateX(' + String(moveData) + 'px';
     }
 }
@@ -32,10 +41,7 @@ function touchStart(event) {
     cardContainer.style.removeProperty('transition');
 }
 
-
 cardContainer.addEventListener('mousedown', touchStart);
 window.addEventListener('mousemove', touchMove);
 window.addEventListener('mouseup', touchEnd);
 window.addEventListener('dragend', touchEnd);
-
-
